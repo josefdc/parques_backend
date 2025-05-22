323b24f2d19b0fa1ebd54773d8b299a7662194ef
Mateo Giraldo Arboleda <mateo.giraldo1@utp.edu.co>
2025-05-22 14:53:30 -0500

    Lo que a duarte le dio pereza

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,10 @@ Este módulo define el modelo de configuración para la aplicación,
 cargando valores desde variables de entorno o valores por defecto.
 """
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Settings(BaseSettings):
     """
@@ -15,6 +19,7 @@ class Settings(BaseSettings):
     """
     PROJECT_NAME: str = "Parqués Backend Distribuido"
     PROJECT_VERSION: str = "0.1.0"
+    ENVIRONMENT : str = os.getenv("ENVIRONMENT")
     # model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='ignore')
 
 settings = Settings()
