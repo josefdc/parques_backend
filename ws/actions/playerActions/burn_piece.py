446de3fd99776ba90ccf3beefc6905247f49c8cb
Mateo Giraldo Arboleda <mateo.giraldo1@utp.edu.co>
2025-05-27 18:28:15 -0500

    quema ficha, doc, color order, broadcast que camilo quiere

--- ws/actions/playerActions/burn_piece.py
+++ ws/actions/playerActions/burn_piece.py
@@ -4,45 +4,38 @@ from fastapi import WebSocket
 from ws.config import API_BASE_URL
 from ws.manager import ConnectionManager
 
-async def handle_move_piece(manager: ConnectionManager, payload: dict, room_id: str, socket: WebSocket):
+async def handle_burn_piece(manager: ConnectionManager, payload: dict, room_id: str, socket: WebSocket):
     try:
         game_id = manager.get_game_id(room_id)
         user_id = manager.get_user_id(socket)
 
         # Extraer datos del payload
         piece_uuid = payload.get("piece_uuid")
-        target_square_id = payload.get("target_square_id")
-        steps_used = payload.get("steps_used")
 
-        if not all([piece_uuid, target_square_id is not None, steps_used is not None]):
-            await manager.send_personal_message("Faltan datos en el movimiento.", socket)
+        if not piece_uuid:
+            await manager.send_personal_message("Falta el piece_uuid", socket)
             return
 
         async with httpx.AsyncClient() as client:
             response = await client.post(
-                f"{API_BASE_URL}/games/{game_id}/move",
+                f"{API_BASE_URL}/games/{game_id}/burn_piece",
                 headers={
                     "accept": "application/json",
                     "Content-Type": "application/json",
                     "x-user-id": user_id
                 },
                 json={
-                    "piece_uuid": piece_uuid,
-                    "target_square_id": target_square_id,
-                    "steps_used": steps_used
+                    "piece_uuid": piece_uuid
                 }
             )
 
         if response.status_code == 200:
             move_data = response.json()
             color = manager.get_user_color(user_id)
-
-
-            move_data.pop("board", None)
             
             await manager.broadcast(
                 json.dumps({
-                    "event": "piece_move_result",
+                    "event": "piece_burn_result",
                     "data": move_data
                 }),
                 room_id
@@ -50,12 +43,12 @@ async def handle_move_piece(manager: ConnectionManager, payload: dict, room_id: 
 
         else:
             await manager.send_personal_message(
-                f"Error al mover la pieza: {response.status_code} - {response.text}",
+                f"Error al quemar la pieza: {response.status_code} - {response.text}",
                 socket
             )
 
     except Exception as e:
         await manager.send_personal_message(
-            f"Excepción en move_piece: {str(e)}",
+            f"Excepción en burn_piece: {str(e)}",
             socket
         )
